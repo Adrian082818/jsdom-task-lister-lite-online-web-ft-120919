b468673baf3d5688a88fd7abfacfcfc8f39fb522
Adrian082818 <mohabiradrian@gmail.com>
2020-03-28 19:57:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const task = document.querySelector("#new-task-description");
+  const form = document.querySelector("#create-task-form");
+  const list = document.querySelector("#tasks");
+  // const priority = document.querySelector("#new-task-priority");
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const newTask = task.value 
+    const newList = document.createElement('li')
+    newList.innerText = newTask
+    list.appendChild(newList)
+
+   const deleteList = document.createElement('button')
+    deleteList.innerText = 'delete'
+    newList.appendChild(deleteList)
+    deleteList.addEventListener('click', function(e) {
+      newList.remove()
+    })
+   });
+  
+
 });
